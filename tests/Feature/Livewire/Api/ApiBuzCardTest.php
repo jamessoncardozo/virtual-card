5a314c322ffaa9bb56a9323e43c4bc15fc77ce55
Jamesson Cardozo <jamessoncardozo@gmail.com>
2023-05-30 19:53:35 +0000

    Anothers Crud Tests

--- tests/Feature/Livewire/Api/ApiBuzCardTest.php
+++ tests/Feature/Livewire/Api/ApiBuzCardTest.php
@@ -12,17 +12,73 @@ use Tests\TestCase;
 
 class ApiBuzCardTest extends TestCase
 {
-  //use RefreshDatabase;
+    //use RefreshDatabase;
+
     /** @test */
-    public function the_api_return_all_users(){
-      
+
+    //sudo php artisan test --filter ApiBuzCardTest::the_api_can_put_one_user
+
+    public function the_api_can_del_one_user(){
+  
       Http::fake();
-      
-      $response=Http::get(route('api.users'));
+      $user=User::factory()->create();
+      dump($user->name);
+      $response = Http::delete('/api/user/41');
 
       $this->assertEquals(true,$response->successful());
 
     }
+
+    //sudo php artisan test --filter ApiBuzCardTest::the_api_can_put_one_user
+
+    public function the_api_can_put_one_user(){
+  
+      Http::fake();
+      $user=User::factory()->create();
+      dump($user->name);
+      $response = Http::put('/api/user/41',['name' => $user->name,]);
+
+      $this->assertEquals(true,$response->successful());
+
+    }
+
+    //sudo php artisan test --filter ApiBuzCardTest::the_api_can_post_one_user
+
+    public function the_api_can_post_one_user(){
+  
+      Http::fake();
+      $user=User::factory()->create();
+      dump($user->name);
+      $response = Http::post('/api/users',['name' => $user->name,]);
+
+      $this->assertEquals(true,$response->successful());
+
+    }
+    //sudo php artisan test --filter ApiBuzCardTest::the_api_return_one_user
+
+    public function the_api_return_one_user(){
+      
+      Http::fake();
+      
+      $response = Http::get('/api/user/15');
+
+      $this->assertEquals(true,$response->successful());
+
+    }
+
+    //sudo php artisan test --filter ApiBuzCardTest::the_api_return_all_users
+
+    public function the_api_return_all_users(){
+      
+      Http::fake();
+      
+      $response = Http::get(route('api.users'));
+
+      $this->assertEquals(true,$response->successful());
+
+    }
+
+
   
     public function the_component_can_render()
     {
